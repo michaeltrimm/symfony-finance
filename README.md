a2679f23b42d9e899974d3df3713613b2e863f14
Michael Trimm <michael@michaeltrimm.com>
2016-12-11 15:26:58 -0500

    Finished implementing sign-in, sign-out, sign-up

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ finance
 =======
 
 A Symfony project created on December 11, 2016, 5:21 pm.
+
+
+## Installation
+
+You must add a new configuration directive inside `app/config/parameters.yml` called `hmac_key` with an alphanum string to encrypt the hash_hmac inputs. The application will fail without this parameter set.
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,32 +8,91 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
+use AppBundle\Entity\User;
+
 
 class UserController extends Controller
 {
   /**
-   * @Route("/user", name="users_index")
+   * @Route("/user", name="user_index")
    * @Method({"GET","HEAD"})
    */
   public function indexAction(Request $request)
   {
     // replace this example code with whatever you need
-    return $this->render('default/index.html.twig', [
-      'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-    ]);
+    return $this->redirectToRoute("user_sign_in");
   }
   
   /**
-   * @Route("/user")
+   * @Route("/user/create")
    * @Method({"POST","PUT"})
    */
   public function createAction(Request $request)
   {
+    // Symfony Objects
+    $em = $this->getDoctrine()->getManager();
+    $session = $request->getSession();
     
+    // Placeholder for any possible error message
+    $err = false;
+    
+    // Input params
+    $email = $request->request->get('email');
+    $password = $request->request->get('password');
+    $confirm = $request->request->get('confirm');
+    
+    // Validate & Sanitize Email Address
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    } else {
+      $err = "Invalid email address!";
+    }
+    
+    // Validate Password Length
+    if(strlen($password) < 6){
+      $err = "Insufficient password length. Must be at least 6 characters!";
+    }
+    
+    // Validate Passwords Match
+    if($password !== $confirm){
+      $err = "Passwords do not match! Please fix before signing up!";
+    }
+    
+    // Respond To Errors
+    if(false !== $err){
+      $request->getSession()->getFlashBag()->add('error',$err);
+      $session->set("sign_up_email",$email);
+      return $this->redirectToRoute('user_sign_up');
+    }
+    
+    // Encrypt Password
+    $encrypted_password = hash_hmac("sha256", $password, $this->container->getParameter('hmac_key'));
+    
+    // Create User Entity
+    $user = new User();
+    $user->setEmail($email);
+    $user->setPassword($encrypted_password);
+    $user->setCreatedOn(new \DateTime());
+    
+    // Save The User
+    $em->persist($user);
+    
+    // Create Authentication Session
+    $session->set("logged_in", true);
+    $session->set("email", $email);
+    
+    // Provide Feedback
+    $request->getSession()->getFlashBag()->add('success','Successfully signed up!');
+    
+    // Make Symfony Happy
+    $em->flush();
+    
+    // Redirect User
+    return $this->redirectToRoute('homepage');
   }
   
   /**
-   * @Route("/user/sign_up", name="users_sign_up")
+   * @Route("/user/sign_up", name="user_sign_up")
    * @Method({"GET","HEAD"})
    */
   public function signupAction(Request $request)
@@ -47,19 +106,91 @@ class UserController extends Controller
    */
   public function signinAction(Request $request)
   {
-    $session = new Session();
-    ob_start();
-    print_r($session);
-    $text = ob_get_clean();
-    return $this->render('user/sign_in.html.twig', ["session" => $text]);
+    return $this->render('user/sign_in.html.twig');
+  }
+  
+  /**
+   * @Route("/user/createSession", name="user_create")
+   * @Method({"POST","PUT"})
+   */
+  public function createSessionAction(Request $request)
+  {
+    // Symfony Objects
+    $em = $this->getDoctrine()->getManager();
+    $session = $request->getSession();
+    
+    // Placeholder for any possible error message
+    $err = false;
+    
+    // Input params
+    $email = $request->request->get('email');
+    $password = $request->request->get('password');
+    
+    // Validate & Santitize Email Address
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    } else {
+      $err = "Invalid email address provided.";
+    }
+    
+    // Validate Password
+    if($password == "" || strlen($password) < 6){
+      $err = "Invalid password provided.";
+    }
+    
+    if(FALSE === $err){
+    
+      // Encrypt Password
+      $encrypted_password = hash_hmac("sha256", $password, $this->container->getParameter('hmac_key'));
+    
+      // Get User Entity
+      $users_table = $this->getDoctrine()->getRepository('AppBundle:User');
+      $user = $users_table->findOneByEmail($email);
+    
+      // Handle No Find Error
+      if(!$user){
+        // No such user, give them a generic response so they can't read between the lines from the response.
+        $err = "No such email address or password.";
+      } else {
+        if(hash_equals($encrypted_password, $user->getPassword())){
+          // Successfully Logged In
+          $session->set("logged_in", true);
+          $session->set("email", $email);
+        } else {
+          // Incorrect Password, Failed To Log In
+          $err = "No such email address or password.";
+        }
+      }
+      
+    }
+    
+    // Were There Errors?
+    if(false !== $err){
+      // Error Trying To Sign In
+      $request->getSession()->getFlashBag()->add('error',$err);
+      $session->set("sign_up_email",$email);
+      return $this->redirectToRoute('user_sign_in');
+    } else {
+      // Successfully Signed In
+      $request->getSession()->getFlashBag()->add('success',"Welcome back ".$email);
+      return $this->redirectToRoute('homepage');
+    }
+    
   }
   
   /**
    * @Route("/user/sign_out", name="user_sign_out")
-   * @Method({"POST","PUT","DELETE"})
+   * @Method({"GET"})
    */
   public function signoutAction(Request $request)
   {
+    $session = $request->getSession();
+    $session->set("logged_in", null);
+    $session->set("email", null);
+    
+    $request->getSession()->getFlashBag()->add('success','Successfully signed out!');
+    
+    return $this->redirectToRoute('user_sign_in');
     
   }
 }
